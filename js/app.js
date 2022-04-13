d9197c25f32078a5f42baed7e15055a1b46767c8
SashaGurin <top-on3@mai.ru>
2022-04-13 15:00:04 +0300

    +video play

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// ======= mobile nav ====================================
 const mobileNavButton = document.querySelector('.mobile-nav-button');
 const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
 const mobileNav = document.querySelector('.mobile__nav');
@@ -9,3 +10,31 @@ mobileNavButton.addEventListener('click', function () {
     mobileNav.classList.toggle('active');
     document.body.classList.toggle('no-scroll')
 })
+
+
+
+// ======= Video ========================================
+const videoFile = document.querySelector('#story-video');
+const videoBtn = document.querySelector('#story-video-btn');
+const videoBtnicon = document.querySelector('#story-video-btn-icon');
+
+videoBtn.addEventListener('click', function () {
+
+    if  (videoFile.paused) {
+        videoFile.play();
+        videoBtnicon.src = "./img/pause_icon.svg";
+    }
+    else {
+        videoFile.pause();
+        videoBtnicon.src = "./img/play-white.svg";
+
+    }
+
+})
+
+
+
+
+
+
+
