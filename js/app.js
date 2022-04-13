6712696007a149617a1a39611680c3464213afd1
SashaGurin <top-on3@mai.ru>
2022-04-13 15:37:23 +0300

    fixed bag_video

--- js/app.js
+++ js/app.js
@@ -1,40 +1,39 @@
 // ======= mobile nav ====================================
-const mobileNavButton = document.querySelector('.mobile-nav-button');
-const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
-const mobileNav = document.querySelector('.mobile__nav');
+const mobileNavButton = document.querySelector(".mobile-nav-button");
+const mobileNavIcon = document.querySelector(".mobile-nav-button__icon");
+const mobileNav = document.querySelector(".mobile__nav");
 
-
-
-mobileNavButton.addEventListener('click', function () {
-    mobileNavIcon.classList.toggle('active');
-    mobileNav.classList.toggle('active');
-    document.body.classList.toggle('no-scroll')
-})
-
-
+mobileNavButton.addEventListener("click", function () {
+  mobileNavIcon.classList.toggle("active");
+  mobileNav.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
 
 // ======= Video ========================================
-const videoFile = document.querySelector('#story-video');
-const videoBtn = document.querySelector('#story-video-btn');
-const videoBtnicon = document.querySelector('#story-video-btn-icon');
+const videoFile = document.querySelector("#story-video");
+const videoBtn = document.querySelector("#story-video-btn");
+const videoBtnicon = document.querySelector("#story-video-btn-icon");
+const videoOverlay = document.querySelector("#story-video-overlay");
 
-videoBtn.addEventListener('click', function () {
-
-    if  (videoFile.paused) {
-        videoFile.play();
-        videoBtnicon.src = "./img/pause_icon.svg";
+videoBtn.addEventListener("click", function () {
+  function toggleOverlay(event) {
+    if (event.type === "mouseleave") {
+      videoOverlay.classList.add("hidden");
+    } else {
+      videoOverlay.classList.remove("hidden");
     }
-    else {
-        videoFile.pause();
-        videoBtnicon.src = "./img/play-white.svg";
+  }
 
-    }
+  if (videoFile.paused) {
+    videoFile.play();
+    videoBtnicon.src = "./img/pause_icon.svg";
 
-})
-
-
-
-
-
-
-
+    videoOverlay.onmouseleave = toggleOverlay;
+    videoOverlay.onmouseenter = toggleOverlay;
+  } else {
+    videoFile.pause();
+    videoBtnicon.src = "./img/play-white.svg";
+    videoOverlay.onmouseleave = null;
+    // videoOverlay.onmouseenter = null;
+  }
+});
